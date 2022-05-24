6198341cc39425fa667aa04ff429befbc141f135
IamFaisal11 <94454129+IamFaisal11@users.noreply.github.com>
2022-05-24 09:14:01 +0530

    Rename triangle33.cpp to Efficiency_of_worker.cpp

--- Efficiency_of_worker.cpp
+++ Efficiency_of_worker.cpp
@@ -1,7 +1,7 @@
 /* In a company, worker efficiency is determined on the basis of the time required for a worker to complete 
-a particular job. If the time taken by the worker is between 2 – 3 hours, then the worker is said to be highly 
-efficient. If the time required by the worker is between 3 – 4 hours, then the worker is ordered to improve speed. 
-If the time taken is between 4 – 5 hours, the worker is given training to improve his speed, and if the time 
+a particular job. If the time taken by the worker is between 2 â€“ 3 hours, then the worker is said to be highly 
+efficient. If the time required by the worker is between 3 â€“ 4 hours, then the worker is ordered to improve speed. 
+If the time taken is between 4 â€“ 5 hours, the worker is given training to improve his speed, and if the time 
 taken by the worker is more than 5 hours, then the worker has to leave the company. If the time taken by the worker 
 is input through the keyboard, find the efficiency of the worker */
 #include<iostream>
